107534690853099e2116cc3032e37064dd594a4d
Kincron <76568146+Kincron@users.noreply.github.com>
2022-12-06 19:02:24 -0300

    Adição de pequenas anotações e melhorias no código

--- comparaNumeros.js
+++ comparaNumeros.js
@@ -1,12 +1,14 @@
 
 function verificaNumeros (num1, num2) {
-
-    if (num1 == num2) {
+    
+    if (num1 === num2) {
         console.log(`Os números ${num1} e ${num2} são iguais. `)
     } else {
         console.log(`Os números ${num1} e ${num2} não são iguais. `)
     }
 
+    //Também pode ser feito: return (num1 === num2) ? `Os números ${num1} e ${num2} são iguais.` : `Os números ${num1} e ${num2} não são iguais`;
+    //Porém quase o mesmo erro acontece de exibir o NaN, porém, é no final da frase e exibe '.undefined'
 }
 
 function verificaSoma (num1, num2) {
@@ -14,9 +16,9 @@ function verificaSoma (num1, num2) {
 
     if (soma > 10 && soma < 20) {
         console.log(`Sua soma é ${soma}, que é maior que 10 e menor que 20.`)
-    } else if (soma == 10) {
+    } else if (soma === 10) {
         console.log(`Sua soma é ${soma}, que é igual a 10 e menor que 20`)
-    } else if (soma == 20) {
+    } else if (soma === 20) {
         console.log(`Sua soma é ${soma}, que é maior que 10 e igual a 20`)
     } else if (soma > 11 && soma > 20) {
         console.log(`Sua soma é ${soma}, que é maior que 10 e maior que 20`)
@@ -25,4 +27,4 @@ function verificaSoma (num1, num2) {
     }
 }
 
-console.log(verificaNumeros(10, 10) + verificaSoma(10, 10))
+console.log(verificaNumeros(10, 10) + verificaSoma(10, 10))  // Ao executar tudo, exibe a expressão NaN, não consegui resolver este problema.
